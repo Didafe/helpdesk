7e651db6dfa4d98ae002a2e6acf8b063432e91a5
didafe <you@example.com>
2024-01-28 23:55:12 -0300

    first commit

---
+++ src/main/java/com/helpdesk/Helpdesk/models/User.java
@@ -0,0 +1,109 @@
+package com.helpdesk.Helpdesk.models;
+
+
+
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    @Email(message = "Please provide a valid email")
+    @NotEmpty(message = "Can not be empty")
+    private String email;
+
+    @Column
+    @NotEmpty(message = "Can not be empty")
+    private String name;
+
+    @Column
+    @NotEmpty(message = "Can not be empty")
+    private String lastName;
+
+    @Column
+    @NotEmpty(message = "Can not be empty")
+    @Length(min = 5, message = "You need to provide a password that contains at least 5 characters")
+    private String password;
+
+    @Column
+    private Boolean active;
+
+    public User() {}
+
+    public User(String email, String name, String lastName, String password, Boolean active) {
+        this.email = email;
+        this.name = name;
+        this.lastName = lastName;
+        this.password = password;
+        this.active = active;
+    }
+
+    public User(Long id, String email, String name, String lastName, String password, Boolean active) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.lastName = lastName;
+        this.password = password;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
